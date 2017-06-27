f1ef0c0c91788342dd024c7d0d1213dc7fc17a80
Austin Conlon <austinconlon@me.com>
2017-06-27 14:27:33 -0700

    start iOS app with WatchKit app project

---
+++ CoinPush WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  CoinPush WatchKit Extension
+//
+//  Created by Austin Conlon on 6/27/17.
+//  Copyright © 2017 Goods and Services. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
