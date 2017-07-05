9f3f7829450479afbe0050348de9e150e8aefd36
Jeffrey Thomas Piercy <mqduck@mqduck.net>
2017-07-05 00:42:28 -0700

    Many changes to make the code not suck.

--- Android/app/src/main/java/net/mqduck/coinpush/CurrencyAdapter.java
+++ Android/app/src/main/java/net/mqduck/coinpush/CurrencyAdapter.java
@@ -2,6 +2,8 @@ package net.mqduck.coinpush;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,41 +11,67 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by mqduck on 7/4/17.
  */
 
-public class CurrencyAdapter extends ArrayAdapter<CurrencyData>
+class CurrencyAdapter extends ArrayAdapter<CurrencyData>
 {
-    private final Context context;
+    //private final Context context;
     private final List<CurrencyData> data;
     private LayoutInflater inflater;
     
-    public CurrencyAdapter(Context context, List<CurrencyData> data)
+    private class UpdateTask extends AsyncTask<CurrencyData, Void, Void>
     {
-        super(context, -1, data);
-        this.context = context;
-        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        this.data = data;
+        @Override
+        protected Void doInBackground(CurrencyData... data)
+        {
+            CurrencyData.updateJson();
+            for(CurrencyData datum : data)
+                datum.update();
+            return null;
+        }
+        
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            notifyDataSetChanged();
+        }
     }
     
-    public View getView(final int position, final View convertView, final ViewGroup parent)
+    CurrencyAdapter(final Context context, final List<CurrencyData> data)
     {
-        View dataView = inflater.inflate(R.layout.currency_layout, parent, false);
+        super(context, -1, data);
+        //this.context = context;
+        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.data = data;
+        
+        new UpdateTask().execute(data.toArray(new CurrencyData[0]));
+    }
+    
+    @NonNull
+    public View getView(final int position, final View convertView, @NonNull final ViewGroup parent)
+    {
+        View dataView;
+        //if(convertView == null)
+            dataView = inflater.inflate(R.layout.currency_layout, parent, false);
+        //else
+        //    dataView = convertView;
         
         TextView textCurrency = (TextView)dataView.findViewById(R.id.textViewCurrency);
         TextView textValue = (TextView)dataView.findViewById(R.id.textViewValue);
         TextView textChange = (TextView)dataView.findViewById(R.id.textViewChange);
         
         CurrencyData datum = data.get(position);
-        textCurrency.setText(datum.getCurrencyName());
-        textValue.setText(String.format("%.3f %s", datum.getValue(), datum.getConversion()));
+        textCurrency.setText(datum.currency.name);
+        textValue.setText(String.format(Locale.getDefault(), "%s %.3f %s", datum.conversion.symbol, datum.getValue(), datum.conversion.code));
         
         double change = datum.getChange();
-        textChange.setText(String.format("%+.4f%%", change));
+        textChange.setText(String.format(Locale.getDefault(), "%+.4f%%", change));
         if(change < 0)
-            textChange.setTextColor(Color.rgb((int)Math.round(255 + change * 30), 0, 0));
+            textChange.setTextColor(Color.rgb((int)Math.round(change * -30), 0, 0));
         else
             textChange.setTextColor(Color.rgb(0, (int)Math.round(change * 30), 0));
         
--- Android/app/src/main/java/net/mqduck/coinpush/CurrencyData.java
+++ Android/app/src/main/java/net/mqduck/coinpush/CurrencyData.java
@@ -1,67 +1,117 @@
+/*
+ * Copyright 2017 Jeffrey Thomas Piercy
+ *
+ * This file is part of CoinPush.
+ *
+ * CoinPush is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CoinPush is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CoinPush.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.mqduck.coinpush;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.HashMap;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by mqduck on 7/4/17.
  */
 
-public class CurrencyData
+class CurrencyData
 {
-    private final static String BASE_URL = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=%s&tsyms=BTC,USD,EUR";
+    private final static String BASE_URL = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=%s&tsyms=%s";
     private final static String DATUM_VALUE = "PRICE";
     private final static String DATUM_CHANGE = "CHANGEPCT24HOUR";
-    
-    final static String CURRENCY_ETHER = "ETH";
+    final static String CURRENCY_ETH = "ETH";
     final static String CURRENCY_BTC = "BTC";
     final static String CURRENCY_USD = "USD";
+    final static String CURRENCY_EUR = "EUR";
+    final static String CURRENCY_GBP = "GBP";
+    final static String CURRENCY_JPY = "JPY";
+    final static String CURRENCY_CNY = "CNY";
+    final static String CURRENCY_LTC = "LTC";
     
-    final static HashMap<String, String> currencyNames;
+    private final static HashMap<String, Currency> currencies;
+    private final static String url;
     
-    final private String url, name, currency, conversion;
-    private JSONObject json = null;
+    final Currency currency, conversion;
     private Double value = 0.0, change = 0.0;
+    private static JSONObject json;
     
     static
     {
-        currencyNames = new HashMap<>();
-        currencyNames.put(CURRENCY_ETHER, "Etherium");
-        currencyNames.put(CURRENCY_BTC, "Bitcoin");
-        currencyNames.put(CURRENCY_USD, "USD");
+        currencies = new HashMap<>();
+        currencies.put(CURRENCY_ETH, new Currency(CURRENCY_ETH, "Etherium (ETH)", "Ξ"));
+        currencies.put(CURRENCY_BTC, new Currency(CURRENCY_BTC, "Bitcoin (BTC)", /*"\u20BF"*/ ""));
+        currencies.put(CURRENCY_USD, new Currency(CURRENCY_USD, "US Dollar (USC)", "$"));
+        currencies.put(CURRENCY_EUR, new Currency(CURRENCY_EUR, "Euro (EUR)", "€"));
+        currencies.put(CURRENCY_GBP, new Currency(CURRENCY_GBP, "British Pound (GBP)", "£"));
+        currencies.put(CURRENCY_JPY, new Currency(CURRENCY_JPY, "Japanese Yen (JPY)", "¥"));
+        currencies.put(CURRENCY_CNY, new Currency(CURRENCY_CNY, "Chinese Yuan (CNY)", "¥"));
+        currencies.put(CURRENCY_LTC, new Currency(CURRENCY_LTC, "Litecoin (LTC)", "Ł"));
+    
+        String codes = "";
+        for(String code : CurrencyData.currencies.keySet())
+            codes += code + ",";
+        url = String.format(BASE_URL, codes, codes);
     }
     
-    CurrencyData(final String currency, final String conversion)
+    CurrencyData(final String currencyKey, final String conversionKey)
     {
-        this.currency = currency;
-        this.conversion = conversion;
-        name = currencyNames.get(currency);
-        //url = URL_PART_1 + currency + URL_PART_2;
-        url = String.format(BASE_URL, currency);
-        update();
+        currency = currencies.get(currencyKey);
+        conversion = currencies.get(conversionKey);
+    }
+    
+    static void updateJson()
+    {
+        try
+        {
+            InputStream is = null;
+            is = new URL(url).openStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            StringBuilder sb = new StringBuilder();
+            int cp;
+            while((cp = rd.read()) != -1)
+                sb.append((char)cp);
+            json = new JSONObject(sb.toString()).getJSONObject("RAW");
+        }
+        catch(IOException | JSONException e)
+        {
+            e.printStackTrace();
+        }
     }
     
     void update()
     {
         try
         {
-            json = new RetrieveJson().execute(url).get();
-            value = json.getJSONObject(currency).getJSONObject(conversion).getDouble(DATUM_VALUE);
-            change = json.getJSONObject(currency).getJSONObject(conversion).getDouble(DATUM_CHANGE);
+            JSONObject mJson = json.getJSONObject(currency.code).getJSONObject(conversion.code);
+            value = mJson.getDouble(DATUM_VALUE);
+            change = mJson.getDouble(DATUM_CHANGE);
         }
-        catch(InterruptedException | ExecutionException | JSONException e)
+        catch(JSONException e)
         {
             e.printStackTrace();
         }
-    
     }
     
-    public String getCurrencyName() { return name; }
-    public String getCurrency() { return currency; }
-    public String getConversion() { return conversion; }
     public double getValue() { return value; }
     public double getChange() { return change; }
 }
