fb37716cb4af3526483fce1843bf5da46753e377
Jeffrey Thomas Piercy <mqduck@mqduck.net>
2017-07-05 20:55:50 -0700

    Move Android project directory. Add toolbar.

---
+++ Android/CoinPush/app/src/main/java/net/mqduck/coinpush/ActivityMain.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2017 Jeffrey Thomas Piercy
+ *
+ * This file is part of CoinPush.
+ *
+ * CoinPush is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CoinPush is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CoinPush.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.mqduck.coinpush;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ActivityMain extends AppCompatActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    
+        ArrayList<CurrencyData> data = new ArrayList<>();
+        data.add(new CurrencyData(CurrencyData.CURRENCY_ETH, CurrencyData.CURRENCY_USD));
+        data.add(new CurrencyData(CurrencyData.CURRENCY_BTC, CurrencyData.CURRENCY_EUR));
+        data.add(new CurrencyData("EUR", "CNY"));
+        data.add(new CurrencyData(CurrencyData.CURRENCY_GBP, CurrencyData.CURRENCY_JPY));
+        data.add(new CurrencyData("LTC", "ETH"));
+        ListView list = (ListView)findViewById(R.id.list);
+        CurrencyAdapter adapter = new CurrencyAdapter(this, data);
+        list.setAdapter(adapter);
+        
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        
+        //noinspection SimplifiableIfStatement
+        if(id == R.id.action_settings)
+        {
+            return true;
+        }
+        
+        return super.onOptionsItemSelected(item);
+    }
+}
