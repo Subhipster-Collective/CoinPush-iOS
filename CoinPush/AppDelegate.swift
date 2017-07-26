bfa43b190af686ce75f1eadce2b51f76249a9e1b
Bijan-Massoumi <bmassoumi@gmail.com>
2017-07-25 23:26:22 -0700

    configure the acceptance of push notifications

--- CoinPush/AppDelegate.swift
+++ CoinPush/AppDelegate.swift
@@ -10,24 +10,51 @@ import UIKit
 import Firebase
 import FirebaseAuth
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+import UserNotifications
+import FirebaseInstanceID
+import FirebaseMessaging
 
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate,
+UNUserNotificationCenterDelegate, MessagingDelegate {
+    /// This method will be called whenever FCM receives a new, default FCM token for your
+    /// Firebase project's Sender ID.
+    /// You can send this token to your application server to send notifications to this device.
+
+
+ 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
         [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Use Firebase library to configure APIs.
-        FirebaseApp.configure()
         // Initialize the Google Mobile Ads SDK.
+
+        
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+            // For iOS 10 data message (sent via FCM
+            Messaging.messaging().remoteMessageDelegate = self as! MessagingDelegate
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        FirebaseApp.configure()
+        
         GADMobileAds.configure(withApplicationID: Passwords.adMobID)
-        
-        
-        
         
         Auth.auth().signInAnonymously() { (user, error) in
             Passwords.userID = user!.uid
         }
+
         
         return true
     }
@@ -53,7 +80,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    //MARK: FIRMessagingDelegate
+    
+    // The callback to handle data message received via FCM for devices running iOS 10 or above.
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        
+        if let messageID = userInfo["gcm_message_id"] {
+            print("Message ID: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+        
+        let alert = UIAlertController(title: "FireBase", message:"didReceiveRemoteNotification", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        
+        // show the alert
+        self.window?.rootViewController?.present(alert, animated: true, completion: nil)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        if let messageID = userInfo["gcm_message_id"] {
+            print("Message ID: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+        
+        
+        let alert = UIAlertController(title: "FireBase", message:"didReceiveRemoteNotification fetchCompletionHandler", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        
+        // show the alert
+        self.window?.rootViewController?.present(alert, animated: true, completion: nil)
+        
+        
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+    func application(received remoteMessage: MessagingRemoteMessage) {
+        print(" APNSData \(remoteMessage.appData)")
+    }
+    
+    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+        print("Firebase registration token: \(fcmToken)")
+    }
+    private func application(application: UIApplication,
+                             didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        Messaging.messaging().apnsToken = deviceToken as Data
+    }
+    
 
 }
 
